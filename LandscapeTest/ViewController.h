165a27e7d9b5387e700935ebd63220411b3c3df5
Sergey Nikitenko <s.nikitenko@me.com>
2012-09-25 21:49:59 +0300

    LandscapeTest

---
+++ LandscapeTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LandscapeTest
+//
+//  Created by onegray on 9/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
